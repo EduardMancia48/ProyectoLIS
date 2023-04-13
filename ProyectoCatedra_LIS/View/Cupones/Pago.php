89a96e26fd8edff63f1faac7c5d1bf48d928be0d
Ludwig Stanley <125160770+RecDwig@users.noreply.github.com>
2023-04-12 19:39:01 -0600

    Add files via upload
    commit del commit del commit

--- ProyectoCatedra_LIS/View/Cupones/Pago.php
+++ ProyectoCatedra_LIS/View/Cupones/Pago.php
@@ -4,6 +4,7 @@
 <head>
 	<title>Credit Card Payment Form</title>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+	<link rel="stylesheet" href="../View/assets/css/estilos.css">
 </head>
 <header>
         <div class="navbar navbar-dark bg-dark shadow-sm">
@@ -11,25 +12,28 @@
                 <a href="#" class="navbar-brand d-flex align-items-center">
                     <strong>Cuponera Treasure</strong>
                 </a>
+				<form class="form-inline my-2 my-lg-0">
+                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit" name="C_sesion">Cerrar sesión</button>
+                </form>
             </div>
         </div>
     </header>
-<body>
+<body background="https://cdn.pixabay.com/photo/2012/04/18/16/09/beehive-37436_640.png">
 	<div class="container mt-5">
-		<h1>Cupones Treasure </h1>
+		<h1><strong>Cupones Treasure</strong> </h1>
 		<form action="/ProyectoCatedra_LIS/View/Cupones/Pago.php" method="post">
-		<div class="col-lg-7 border shadow pb-3 pt-3">
+		<div class="col-lg-7 border shadow pb-3 pt-3 bg-dark">
 			
 		<div class="form-group row">
 			<div class="col-lg-10">
-				<label for="card_number"><b>Nombre del Titular:</b></label>
+				<label for="card_number" class="text-white"><b>Nombre del Titular:</b></label>
 				<input type="text" class="form-control" name="user_name" id="user_name" pattern="[a-zA-Z]+" placeholder="Ingrese su nombre"required>
 			</div>
 		</div>
 
 		<div class="form-group row">
 			<div class="col-lg-9">
-				<label for="card_number"><b>Número de Tarjeta:</b></label>
+				<label for="card_number" class="text-white"><b>Número de Tarjeta:</b></label>
 				<input type="text" class="form-control" name="card_number" id="card_number" placeholder="Código de tarjeta" pattern="^(?:4[0-9]{12}(?:[0-9]{3})?|5[1-5][0-9]{14}|6(?:011|5[0-9][0-9])[0-9]{12}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|(?:2131|1800|35\d{3})\d{11})$" required>
 			</div>
 		</div>
@@ -37,14 +41,14 @@
 		
 		<div class="form-group row">
 			<div class="col-lg-5">
-				<label for="expiry_date"><b>Fecha de expiración (MM/YY):</b></label>
+				<label for="expiry_date" class="text-white"><b>Fecha de expiración (MM/YY):</b></label>
 				<input type="text" class="form-control" name="expiry_date" id="expiry_date" placeholder="Fecha de expiración" pattern="^(0[1-9]|1[0-2])\/([0-9]{2})$" required>
 			</div>
 		</div>
 
 		<div class="form-group row">
 			<div class="col-lg-4">
-				<label for="cvv"><b>CVV:</b></label>
+				<label for="cvv" class="text-white"><b>CVV:</b></label>
 				<input type="text" class="form-control" name="cvv" id="cvv" placeholder="CVV"pattern="^\d{3,4}$" required>
 			</div>
 		</div>
--- ProyectoCatedra_LIS/View/Cupones/index.php
+++ ProyectoCatedra_LIS/View/Cupones/index.php
@@ -7,29 +7,37 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cupones</title>
     <link rel="stylesheet" href="https://preview.colorlib.com/theme/bootstrap/login-form-14/css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="./View/assets/css/estilos.css">
 </head>
-<body>
+<body> 
     <header>
         <div class="navbar navbar-dark bg-dark shadow-sm">
             <div class="container">
                 <a href="#" class="navbar-brand d-flex align-items-center">
-                    <strong>Cuponera Treasure</strong>
+                    <h2><strong class="text-white">Cuponera Treasure</strong></h2>
                 </a>
+                <form class="form-inline my-2 my-lg-0">
+                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit" name="C_sesion">Cerrar sesión</button>
+                </form>
             </div>
         </div>
     </header>
     <main>
-        <div class="py-5 bg-light">
+        <div class="py-5 bg-light border rounded shadow">
             <div class="container">
                 <h3><b>Descubre los tesoros disponibles </b></h3>
                 <center>
+                <div class="container">
                 <div class="row">
                     <div class="col mb-5">
-                        <img src="\ProyectoCatedra_LIS\View\assets\images\logoweb.png" width="500px" height="500px" class="shadow rounded">
+                        <img src="\ProyectoCatedra_LIS\View\assets\images\logoweb.png" width="300px" height="300px" class="shadow rounded">
                     </div>
                 </div>
+                </div>
                 </center>
+                <div class="container">
                 <form name="frmTienda" action="/ProyectoCatedra_LIS/View/Cupones" method="POST">
                     <div class="row row-cols-1 row-cols-sm-2 row-cols-md-2 g-4">
                          <?php
@@ -85,11 +93,12 @@
                     ?>
                     </div>
                 </form>
+                </div>
             </div>
         </div>
     </main>
     <p class="float-end mb-1 pr-5">
-                <a href="#">Regresar al inicio</a>
+                <a class="btn btn-warning" href="#">Regresar al inicio</a>
             </p>
 
     <footer class="text-muted py-5">
