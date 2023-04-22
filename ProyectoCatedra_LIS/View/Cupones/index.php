626566b21f1f970d33c7c24acbd9314a4674ced5
Ludwig Stanley <125160770+RecDwig@users.noreply.github.com>
2023-04-21 18:53:32 -0600

    Agregados filtros
    Agregados los filtros de busqueda y filtrado de fechas

--- ProyectoCatedra_LIS/View/Cupones/index.php
+++ ProyectoCatedra_LIS/View/Cupones/index.php
@@ -8,7 +8,7 @@
     <title>Cupones</title>
     <link rel="stylesheet" href="https://preview.colorlib.com/theme/bootstrap/login-form-14/css/style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    
     <link rel="stylesheet" href="./View/assets/css/estilos.css">
 </head>
 <body> 
@@ -19,80 +19,94 @@
                     <h2><strong class="text-white">Cuponera Treasure</strong></h2>
                 </a>
                 <form class="form-inline my-2 my-lg-0">
-                <a class="btn btn-success" name="C_sesion" href="/ProyectoCatedra_LIS/Usuarios/logout/">Cerrar sesión</a>                
+                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit" name="C_sesion">Cerrar sesión</button>
                 </form>
+                
             </div>
         </div>
     </header>
-    <main>
-        <div class="py-5 bg-light border rounded shadow">
+    <main>     
+    <div class="py-5 bg-light border rounded shadow">
             <div class="container">
-                <h3><b>Descubre los tesoros disponibles </b></h3>
+                </div>
                 <center>
                 <div class="container">
                 <div class="row">
                     <div class="col mb-5">
                         <img src="\ProyectoCatedra_LIS\View\assets\images\logoweb.png" width="300px" height="300px" class="shadow rounded">
                     </div>
+                <div class="row">
+                <h3><b>Descubre los tesoros disponibles </b></h3>  
+                <center>
+                    <div class="input-group mb-3 w-50" >
+                    <input type="text" class="form-control" placeholder="Buscar cupones" aria-label="" aria-describedby="basic-addon1" name="inp_Buscar" id="searchInput">
+                    </div>
+                </center>
+                </div>
                 </div>
                 </div>
                 </center>
                 <div class="container">
-                <form name="frmTienda" action="/ProyectoCatedra_LIS/View/Cupones" method="POST">
-                    <div class="row row-cols-1 row-cols-sm-2 row-cols-md-2 g-4">
-                         <?php
-                             foreach($cupones as $cupon):
-                                  $codigo=$cupon['cod_cupon'];
-                                  $titulo=$cupon['titulo_oferta'];
-                                  $precior=$cupon['precio_regular'];
-                                  $precioOf=$cupon['precio_oferta'];
-                                  $fechai=$cupon['fecha_inicio'];
-                                  $fechaf=$cupon['fecha_fin'];
-                                  $fechal=$cupon['fecha_limite'];
-                                  $desc=$cupon['descripcion'];
-                                  $est=$cupon['estado'];
-                                  $cantd=$cupon['cant_cupones_disponibles'];
-    
-                         ?>
-                        <div class="col">
-                            <div class="card shadow-sm">
-                                <img src="https://www.cuponstar.com/hubfs/Bonda/Images/Mock-Cat%C3%A1logo-MX-1.png" width="100%" height="325" />
-                                <div class="card-body">
-                                    <h5 class="card-title"><?php echo $titulo?></h5>
-                                    <p class="card-text">
-                                        <b> Código:</b>
-                                        <?php echo $codigo; ?> <br />
-                                        <b>Descripción: </b>
-                                        <?php echo $desc; ?><br />
-                                        
-                                        <?php echo "<b>Precio regular: </b>".$precior."$ <b>Precio de oferta: </b>".$precioOf."$"; ?><br />
-                                        <?php echo "<b>Fecha de Inicio: </b>".$fechai." <b>Fecha de finalización: </b>".$fechaf; ?><br />
-                                        <?php echo "<b>Fecha de Inicio: </b>".$fechal?><br />
-                                        <?php echo "<b>Cantidad disponible: </b>".$cantd?><br />
-                                        
-                                        <b>Estado:</b>
-                                        <?php echo $est; ?><br />
-                                        <b>Cantidad a comprar:</b><br />
-
-                                        <?php echo "<input name=\"txtCantidad\" type=\"number\" class=\"form-control\"
-                                            max=\"$cantd\" min=\"0\" step=\"1\" value=\"0\" style=\"width:20%\">" ?>
-                                       
-                                        <div class="row row-cols-1 row-cols-sm-2">
-                                            <div class="col-12 py-4">
-                                                <a class="btn btn-success" name="Comprar_Cupon" href="<?='/ProyectoCatedra_LIS/Cupones/comprar/'.$codigo?>">Comprar</a>
+                    <form name="frmTienda" action="/ProyectoCatedra_LIS/View/Cupones" method="POST">
+                        <div class="row row-cols-2 row-cols-sm-2 row-cols-md-2 g-5" id="cupones_container">
+                            <?php
+                                foreach($cupones as $cupon):
+                                    $codigo=$cupon['cod_cupon'];
+                                    $titulo=$cupon['titulo_oferta'];
+                                    $precior=$cupon['precio_regular'];
+                                    $precioOf=$cupon['precio_oferta'];
+                                    $fechai=$cupon['fecha_inicio'];
+                                    $fechaf=$cupon['fecha_fin'];
+                                    $fechal=$cupon['fecha_limite'];
+                                    $desc=$cupon['descripcion'];
+                                    $est=$cupon['estado'];
+                                    $cantd=$cupon['cant_cupones_disponibles'];
+        
+                            ?>
+                            <div class="col cupon-card">
+                                <div class="card shadow-sm">
+                                    <img src="https://www.cuponstar.com/hubfs/Bonda/Images/Mock-Cat%C3%A1logo-MX-1.png" width="100%" height="325" />
+                                    <div class="card-body">
+                                        <h5 class="card-title"><?php echo $titulo?></h5>
+                                        <p class="card-text" data-fecha-inicio="<?php echo $fechai; ?>" data-fecha-fin="<?php echo $fechaf;?>">
+                                        <a class="btn btn-sm-2 btn-link" data-bs-toggle="collapse" data-bs-target="#collapse1"><b>Mas detalles ⬇️</b></a>
+                                            <div class="mt-2 collapse card shadow pl-2 pb-2" id="collapse1">
+                                            <b> Código:</b>
+                                            <?php echo $codigo; ?> <br />
+                                            <b>Descripción: </b>
+                                            <?php echo $desc; ?><br />
+                                            <?php echo "<b>Precio regular: </b>".$precior."$ <b>Precio de oferta: </b>".$precioOf."$"; ?><br />
+                                            <?php echo "<b>Fecha de Inicio: </b>".$fechai; ?><br />
+                                            <?php echo "<b>Fecha limite: </b>".$fechal?><br />
                                             </div>
-                                        </div>
-                 
-                                    </p>
+                                            <?php echo "<b>Cantidad disponible: </b>".$cantd?><br />
+                                            <b>Estado:</b>
+                                            <?php echo $est; ?><br />
+                                            <b>Cantidad a comprar:</b><br />
+
+                                            <?php echo "<input name=\"txtCantidad\" type=\"number\" class=\"form-control\"
+                                                max=\"$cantd\" min=\"0\" step=\"1\" value=\"0\" style=\"width:20%\">" ?>
+                                        
+                                            <div class="row row-cols-1 row-cols-sm-2">
+                                                <div class="col-12 py-4">
+                                                    <a class="btn btn-success" name="Comprar_Cupon" href="<?='/ProyectoCatedra_LIS/Cupones/comprar/'.$codigo?>">Comprar</a>
+                                                </div>
+                                            </div>
+                    
+                                        </p>
+                                    </div>
                                 </div>
                             </div>
+                            <?php
+                        endforeach;
+                        ?>
                         </div>
-
-                        <?php
-                    endforeach;
-                    ?>
+                    </form>
+                    <div class="row">
+                        <div class="col">
+                            <h2 id="jsMensaje"></p>
+                        </div>
                     </div>
-                </form>
                 </div>
             </div>
         </div>
@@ -106,17 +120,13 @@
         <div class="text-center p-3">
              © 2023 Copyright:
              <p class="mb-1 text-white">Cuponera &copy;</p>
-             
+      
         </div>
         </div>
     </footer>
-
-
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
-        integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">
-    </script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js"
-        integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD" crossorigin="anonymous">
-    </script>
+    <script src="View\assets\js\buscar.js"></script>
+    <script src="View\assets\js\comp_Fechas.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>  
 </body>
 </html>
